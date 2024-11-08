53f3f74619253ff98a02c956425a5a92ab75dbad
Paola <105376831+PaolaUniSa@users.noreply.github.com>
2024-11-08 15:02:25 +0100

    Update NeuronWithDelays_debug.v [skip ci]

--- src/NeuronWithDelays_debug.v
+++ src/NeuronWithDelays_debug.v
@@ -1,26 +1,24 @@
-module NeuronWithDelays_debug #( //weight bit-length=2bits=(zero,sign) // membrane potential bit-length=6 bits
-    parameter M = 8                // Number of input spikes and weights
-)(
+module NeuronWithDelays_debug (
     input wire clk,                      // Clock signal
     input wire reset,                    // Asynchronous reset, active high
     input wire enable,                   // Enable input for the entire neuron
     input wire delay_clk,                // Delay Clock signal
-    input wire [M-1:0] input_spikes,     // M-bit input spikes
-    input wire [M*2-1:0] weights,        // M Nbit weights
+    input wire [8-1:0] input_spikes,     // M-bit input spikes
+    input wire [8*2-1:0] weights,        // M Nbit weights
     input wire [5-1:0] threshold,          // Firing threshold (V_thresh)
     input wire [3-1:0] decay,              // Decay value
     input wire [5-1:0] refractory_period,  // Refractory period in number of clock cycles
-    input wire [M*3-1:0] delay_values,   // Flattened array of 3-bit delay values
-    input wire [M-1:0] delays,           // Array of delay enables for each input
+    input wire [8*3-1:0] delay_values,   // Flattened array of 3-bit delay values
+    input wire [8-1:0] delays,           // Array of delay enables for each input
     output wire [5-1:0] membrane_potential_out, // add for debug
     output wire spike_out                // Output spike signal
 );
-    wire [M-1:0] delayed_spikes;     // Delayed input spikes
+    wire [8-1:0] delayed_spikes;     // Delayed input spikes
 
     // Generate neuron_delay instances for each input spike
     genvar i;
     generate
-        for (i = 0; i < M; i = i + 1) begin: neuron_delay_gen
+        for (i = 0; i < 8; i = i + 1) begin: neuron_delay_gen
             neuron_delay neuron_delay_inst (
                 .sys_clk(clk),
                 .reset(reset),
@@ -35,9 +33,7 @@ module NeuronWithDelays_debug #( //weight bit-length=2bits=(zero,sign) // membra
     endgenerate
 
     // Instantiate the LIF_Neuron module
-    LIF_Neuron_debug #(
-        .M(M)
-    ) lif_neuron_inst (
+    LIF_Neuron_debug lif_neuron_inst (
         .clk(clk),
         .reset(reset),
         .enable(enable),
